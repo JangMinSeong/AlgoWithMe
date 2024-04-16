855f615d15a2bbb943c808fea68c1a23fc194428
JIYEON KIM <jiyeon2536@naver.com>
2024-04-16 18:00:40 +0900

    [FE] 서치바 자리잡기

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import NextAuthProvider from '@/context/NextAuthProvider'
 import UserProvider from '@/context/UserProvider'
 
+import SearchBar from '@/components/layout/searchbar/SearchBar'
+
 export const metadata: Metadata = {
 	title: 'AlgoWithMe',
 	description: '알고리즘 스터디 통합 관리 플랫폼',
@@ -18,7 +20,10 @@ export default function RootLayout({
 		<html lang='kr'>
 			<body>
 				<UserProvider>
-					<NextAuthProvider>{children}</NextAuthProvider>
+					<NextAuthProvider>
+						<SearchBar />
+						{children}
+					</NextAuthProvider>
 				</UserProvider>
 			</body>
 		</html>
---
+++ front/components/layout/searchbar/SearchBar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+	return (
+		<div className='h-10 bg-primary'>
+			<div>서치바</div>
+		</div>
+	)
+}
+
+export default SearchBar
