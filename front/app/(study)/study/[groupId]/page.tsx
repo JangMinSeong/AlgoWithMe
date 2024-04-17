705623928277526b8029ee4c68c6ba58dc44f1d8
JIYEON KIM <jiyeon2536@naver.com>
2024-04-17 17:56:57 +0900

    [FE] Feat : 문제 컴포넌트 뼈대

--- front/app/(study)/study/[groupId]/page.tsx
+++ front/app/(study)/study/[groupId]/page.tsx
@@ -1,10 +1,11 @@
+import Problem from '@/components/studypage/Problem'
 const StudyMainPage = () => {
 	return (
 		<div>
 			스터디그룹의 메인페이지
 			<div>대기방입장하기</div>
 			<div>파이차트</div>
-			<div>풀어볼 문제</div>
+			<Problem />
 			<div>랭킹</div>
 		</div>
 	)
--- front/components/studypage/Problem.tsx
+++ front/components/studypage/Problem.tsx
@@ -1,14 +1,19 @@
 import Image from 'next/image'
-import Link from 'next/link'
+import { BiLinkExternal } from 'react-icons/bi'
+
 const Problem = () => {
 	return (
-		<div>
-			<Image src='/bojlogo.png' alt='백준로고' width={40} height={40} className='rounded-full' />
-			<div>
-				{'5534.'}
-				{'민숭의 생일파티'}
+		<div className='p-[2px] bg-gradient-to-br w-fit from-primary via-secondary to-blueishPurple rounded-lg '>
+			<div className='flex bg-background w-fit items-center px-4 py-4 rounded-lg'>
+				<Image src='/bojlogo.png' alt='백준로고' width={40} height={40} className='rounded-full mr-2 ' />
+				<div className='mr-2 w-60'>
+					{'5534.'}
+					{'민숭의 생일파티'}
+				</div>
+				<a target='_blank' href={'https://www.acmicpc.net/problem/1000'}>
+					<BiLinkExternal />
+				</a>
 			</div>
-			<a target='_blank' href={'https://www.acmicpc.net/problem/1000'}></a>
 		</div>
 	)
 }
