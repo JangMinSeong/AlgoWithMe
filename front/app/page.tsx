85c96b248db1b696c0e9887be2c1aebd3711baf5
조현우 <chw03177@gmail.com>
2024-04-18 10:27:39 +0900

    [FE] Feat: 로고 추가

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,24 +1,25 @@
-import * as React from "react";
-import Button from "@/components/Button";
-import LoginButton from "@/components/LoginButton";
+import * as React from 'react'
+import Button from '@/components/Button'
+import LoginButton from '@/components/LoginButton'
+import Logo from '@/components/Logo'
 
 export default function LandingPage() {
   return (
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold text-center text-gray-800 mb-4">
-          랜딩 페이지
-        </h1>
-        <p className="text-xl text-center text-gray-600 mb-8">
-          lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-        <div className="flex gap-4">
-          <Button>시작하기</Button>
-            <LoginButton />
-          <button
-              className="px-6 py-3 bg-transparent text-blue-500 font-semibold rounded-lg border border-blue-500 hover:bg-blue-500 hover:text-white transition-colors">
-            깃 허브로 로그인
-          </button>
-        </div>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold text-center text-gray-800 mb-4">
+        랜딩 페이지
+      </h1>
+      <p className="text-xl text-center text-gray-600 mb-8">
+        lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <div className="flex gap-4">
+        <Button>시작하기</Button>
+        <LoginButton />
+        <button className="px-6 py-3 bg-transparent text-blue-500 font-semibold rounded-lg border border-blue-500 hover:bg-blue-500 hover:text-white transition-colors">
+          깃 허브로 로그인
+        </button>
       </div>
-  );
+      <Logo />
+    </div>
+  )
 }
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -13,14 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#c485ee",
+        secondary: "#f4aeb2",
+        accent: "#f7bafc",
+        text: "#06010E",
+        background: "#F2F2F2"
+      }
     },
-    colors: {
-      primary: "#c485ee",
-      secondary: "#f4aeb2",
-      accent: "#f7bafc",
-      text: "#06010E",
-      background: "#F2F2F2"
-    }
   },
   plugins: [],
 };
