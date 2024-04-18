25aff1cd7767599e232636c920ac904956728e25
조현우 <chw03177@gmail.com>
2024-04-18 13:27:34 +0900

    [FE] Feat: 컨벤션 규칙 수정

---
+++ front/app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import SideBar from '@/components/layout/SideBar'
+import SidebarProvider from '@/context/SidebarProvider'
+import React from 'react'
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <SidebarProvider>
+      <div className="flex">
+        <SideBar />
+        <main>{children}</main>
+      </div>
+    </SidebarProvider>
+  )
+}
