dbb7f308e3f57e5764e32397e0c57543fb6ff63a
장민성 <hs0765@naver.com>
2024-04-18 16:02:27 +0900

    [FE] Feat: pageEditor test

--- front/components/Header/Header.tsx
+++ front/components/Header/Header.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import SearchDropdown from '@/components/Header/SearchDropdown'
 
 const MainHeader: React.FC = () => {
-  const [dropdownVisible, setDropdownVisible] = useState<boolean>(false)
   interface Item {
     id: number
     description: string
@@ -16,17 +15,12 @@ const MainHeader: React.FC = () => {
     { id: 3, description: '스터디 3' },
   ]
 
-  const handleItemClick = (itemName: string) => {
-    console.log(`${itemName} 클릭됨!`)
-    setDropdownVisible(false)
-  }
-
   return (
     <header className="w-full flex justify-center items-center p-2 shadow">
       <div className="flex-none w-1/4">
         <div className="text-xl font-bold">LOGO</div>
       </div>
-      <SearchDropdown items={items} handleItemClick={handleItemClick} />
+      <SearchDropdown items={items} />
       <div className="flex-none w-1/4 text-right">profile</div>
     </header>
   )
