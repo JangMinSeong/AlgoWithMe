3470f41732ecd914b43cf431db2e8bcff3d9a01a
조현우 <chw03177@gmail.com>
2024-04-23 00:49:22 +0900

    [FE] Feat: 로그인 기능 구현중

--- front/app/loading/page.tsx
+++ front/app/loading/page.tsx
@@ -2,15 +2,33 @@
 
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
+import axios from 'axios'
 
 export default function Loading() {
   const searchParams = useSearchParams()
   const router = useRouter()
-  const { code } = searchParams.get('code')
+  const code = searchParams.get('code')
+
+  const loginWithCode = async () => {
+    try {
+      const response = await axios.post('/user/login', { code })
+
+      if (response.status === 200 && response.data.success) {
+        router.push('/main')
+      } else {
+        router.push('/')
+      }
+    } catch (error) {
+      router.push('/')
+    }
+  }
 
   useEffect(() => {
     if (!code) {
       router.push('/')
+      return
     }
-  }, [])
+
+    loginWithCode()
+  }, [code, router])
 }
