5425bdc128b320fcb2644a40cb4c89bfbfdac441
조현우 <chw03177@gmail.com>
2024-04-23 15:16:46 +0900

    [FE] Feat: 랜딩페이지 제작중

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -5,12 +5,17 @@ export default function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col">
       <LandingHeader />
-      <h1 className="text-5xl font-bold text-center text-gray-800 mb-4">
-        랜딩 페이지
-      </h1>
-      <p className="text-xl text-center text-gray-600 mb-8">
-        lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+
+      <div className="2xl:container 2xl:mx-auto pt-24 px-16">
+        <div className="flex flex-col">
+          <h1 className="break-keep text-balance text-8xl font-bold text-darkNavy mb-6">
+            알고리즘 스터디 여기서 함께
+          </h1>
+          <p className="break-keep text-3xl mb-8 text-navy">
+            실시간 온라인 알고리즘 스터디 플랫폼
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
