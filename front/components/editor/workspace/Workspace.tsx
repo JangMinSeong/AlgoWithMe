489b8016ef28282587d1a3149ed982429acb4848
장민성 <hs0765@naver.com>
2024-04-24 16:50:07 +0900

    [FE] Feat: 개인 메모장 연결, 코드에디터 추가

--- front/components/editor/workspace/Workspace.tsx
+++ front/components/editor/workspace/Workspace.tsx
@@ -2,170 +2,25 @@
 
 import '@/components/editor/workspace/styles.scss'
 
-import React, { useCallback, useEffect, useState } from 'react'
-import { EditorContent, useEditor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import Highlight from '@tiptap/extension-highlight'
-import TaskItem from '@tiptap/extension-task-item'
-import TaskList from '@tiptap/extension-task-list'
-import { Color } from '@tiptap/extension-color'
-import TextStyle from '@tiptap/extension-text-style'
-import { Table } from '@tiptap/extension-table'
-import { TableRow } from '@tiptap/extension-table-row'
-import { FontSize } from 'tiptap-extension-font-size'
-import { TableCell } from '@tiptap/extension-table-cell'
-import { TableHeader } from '@tiptap/extension-table-header'
-import Placeholder from '@tiptap/extension-placeholder'
-import Details from '@tiptap-pro/extension-details'
-import DetailsSummary from '@tiptap-pro/extension-details-summary'
-import DetailsContent from '@tiptap-pro/extension-details-content'
-import Image from '@tiptap/extension-image'
-import CharacterCount from '@tiptap/extension-character-count'
-import Collaboration from '@tiptap/extension-collaboration'
-import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
-import * as Y from 'yjs'
-import { TiptapCollabProvider } from '@hocuspocus/provider'
+import React from 'react'
+import { Editor, EditorContent } from '@tiptap/react'
 
 import MenuBar from '@/components/editor/workspace/MenuBar'
-import Button from '@/components/Button'
 
-const colors = [
-  '#958DF1',
-  '#F98181',
-  '#FBBC88',
-  '#FAF594',
-  '#70CFF8',
-  '#94FADB',
-  '#B9F18D',
-]
-const names = [
-  'Lea Thompson',
-  'Cyndi Lauper',
-  'Tom Cruise',
-  'Madonna',
-  'Jerry Hall',
-  'Joan Collins',
-  'Winona Ryder',
-  'Christina Applegate',
-  'Alyssa Milano',
-  'Molly Ringwald',
-  'Ally Sheedy',
-  'Debbie Harry',
-  'Olivia Newton-John',
-  'Elton John',
-  'Michael J. Fox',
-  'Axl Rose',
-  'Emilio Estevez',
-  'Ralph Macchio',
-  'Rob Lowe',
-  'Jennifer Grey',
-  'Mickey Rourke',
-  'John Cusack',
-  'Matthew Broderick',
-  'Justine Bateman',
-  'Lisa Bonet',
-]
-
-const getRandomElement = <T,>(list: T[]): T =>
-  list[Math.floor(Math.random() * list.length)]
-const getRandomColor = (): string => getRandomElement(colors)
-const getRandomName = (): string => getRandomElement(names)
-
-const appId = process.env.NEXT_PUBLIC_TIPTAP_ID as string
-
-const ydoc = new Y.Doc()
-const websocketProvider = new TiptapCollabProvider({
-  appId,
-  name: 'test2', // 이름으로 문서 분류 함
-  document: ydoc,
-})
-
-interface User {
-  name: string
-  color: string
+interface WorkSpaceProps {
+  editor: Editor
 }
 
-const getInitialUser = (): User => ({
-  name: getRandomName(),
-  color: getRandomColor(),
-})
-
-const WorkSpace: React.FC = () => {
-  const [status, setStatus] = useState('connecting')
-  const [currentUser, setCurrentUser] = useState(getInitialUser)
-
-  const editor = useEditor({
-    extensions: [
-      StarterKit.configure({ history: false }),
-      Highlight,
-      TaskList,
-      TaskItem,
-      Table.configure({
-        resizable: true,
-      }),
-      TableRow,
-      TableCell,
-      TableHeader,
-      Image,
-      Color,
-      TextStyle,
-      FontSize,
-      Details.configure({
-        persist: true,
-        HTMLAttributes: {
-          class: 'details',
-        },
-      }),
-      DetailsSummary,
-      DetailsContent,
-      Placeholder.configure({
-        includeChildren: true,
-        placeholder: ({ node }) => {
-          if (node.type.name === 'detailsSummary') {
-            return '제목'
-          }
-          return null
-        },
-      }),
-      CharacterCount.configure({ limit: 10000 }),
-      Collaboration.configure({ document: ydoc }),
-      CollaborationCursor.configure({ provider: websocketProvider }),
-    ],
-  })
-
-  useEffect(() => {
-    websocketProvider.on('status', (event) => {
-      setStatus(event.status)
-    })
-  }, [websocketProvider])
-
-  useEffect(() => {
-    if (editor) {
-      editor.chain().focus().updateUser(currentUser).run()
-    }
-  }, [editor, currentUser])
-
-  const setName = useCallback(() => {
-    const name = (window.prompt('Name') || '').trim().substring(0, 32)
-    if (name) {
-      setCurrentUser({ ...currentUser, name })
-    }
-  }, [currentUser])
-
-  return (
-    <div className="editor">
-      {editor && <MenuBar editor={editor} />}
-      <EditorContent className="editor__content" editor={editor} />
-      <div className="editor__footer">
-        <div className={`editor__status editor__status--${status}`}>
-          {status === 'connected' ? `Online` : 'Offline'}
-        </div>
-        <div className="editor__name">
-          <Button onClick={setName}>{currentUser.name}</Button>
-        </div>
-      </div>
-    </div>
-  )
-}
+const WorkSpace: React.FC<WorkSpaceProps> = ({ editor }) => (
+  <div className="editor">
+    {editor && <MenuBar editor={editor} />}
+    <EditorContent className="editor__content" editor={editor} />
+    {/* <div className="editor__footer"> */}
+    {/*  <div className={`editor__status editor__status--${status}`}> */}
+    {/*    {status === 'connected' ? `Online` : 'Offline'} */}
+    {/*  </div> */}
+    {/* </div> */}
+  </div>
+)
 
 export default WorkSpace
