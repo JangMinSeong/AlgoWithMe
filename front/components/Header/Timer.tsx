ca5c16bb448627324610aa84ab0c01eeed68a422
JIYEON KIM <jiyeon2536@naver.com>
2024-04-26 14:06:34 +0900

    [FE] Fix : 타이머 음수 에러 수정

--- front/components/Header/Timer.tsx
+++ front/components/Header/Timer.tsx
@@ -6,57 +6,43 @@ import { useEffect, useState } from 'react'
 const Timer = () => {
   const timer = useSelector((state: RootState) => state.timer)
 
-  // 설정된 시간
-  let initialHour = timer.hour
-  let initialMin = timer.min
+  const studyDurationInSec = timer.hour * 3600 + timer.min * 60 // 스터디 진행시간 in S
 
-  if (timer.min === 0) {
-    initialMin = 59
-    initialHour -= 1
-  } else {
-    initialMin -= 1
-  }
+  const [remainHour, setRemainHour] = useState(timer.hour)
+  const [remainMin, setRemainMin] = useState(timer.min)
+  const [remainSec, setRemainSec] = useState(0)
 
-  // 만약 분이 0으로 시작되면, initialhour에서 -1 하고 분을 59로 만들어줘야함
-
-  const [remainHour, setRemainHour] = useState(initialHour) // 현재 시간 상태 초기화
-  const [remainMin, setRemainMin] = useState(initialMin) // 현재 시간 상태 초기화
-  const [remainSec, setRemainSec] = useState(59)
   // 스터디 시작 시간
   const dummyStartTime = new Date(2024, 3, 26, 8, 0).getTime()
-  // 기점부터 경과된 밀리초
 
   useEffect(() => {
     const timerID = setInterval(() => {
       const localTime = new Date().getTime()
       // 매 초마다 현재 로컬 시간 업데이트
 
-      const timeDiffinMS = localTime - dummyStartTime
-      // 밀리세컨드를 시간, 분, 초로 변환
+      const totalPassedSec = Math.floor((localTime - dummyStartTime) / 1000) // 전체 경과 in S
 
-      // 1시간 : 3600000 ms == 3600s
-      // 1분 : 60000 ms == 60s
-      // 1초 : 1000 ms == 1s
+      // 총 남은 초
+      const totalRemainSec = studyDurationInSec - totalPassedSec
 
-      const totalPassedSec = Math.floor(timeDiffinMS / 1000) // 전체 경과 in S
+      if (totalRemainSec <= 0) {
+        clearInterval(timerID)
+      }
 
-      const hourPassed = Math.floor(totalPassedSec / 3600) // 경과 초에서 3600 나눈 몫
-      const minPassed = Math.floor((totalPassedSec - hourPassed * 3600) / 60) // 경과 초에서 (시간*초)을 빼고, 60으로 나눈 몫
-      const secPassed = totalPassedSec % 60 // 경과 초
+      // 남은 초를 시간 분 초 단위로 변경
+      const newRemainSec = totalRemainSec % 60 // 나머지 == 초
+      const newRemainMin = Math.floor((totalRemainSec / 60) % 60)
+      const newRemainHour = Math.floor(totalRemainSec / 3600)
 
-      const newRemainHour = Math.max(0, initialHour - hourPassed)
-      const newRemainMin = Math.max(0, initialMin - minPassed)
-      const newRemainSec = Math.max(0, 59 - secPassed)
-
-      setRemainHour(newRemainHour) // 매 분마다 현재 시간 업데이트
-      setRemainMin(newRemainMin)
-      setRemainSec(newRemainSec)
-    }, 1000) // 1분 간격으로 setInterval 실행
+      setRemainHour(Math.max(0, newRemainHour))
+      setRemainMin(Math.max(0, newRemainMin))
+      setRemainSec(Math.max(0, newRemainSec))
+    }, 1000) // 1초 간격으로 setInterval 실행
 
     return () => {
       clearInterval(timerID) // 컴포넌트가 언마운트될 때 인터벌 정리
     }
-  }, [initialHour, initialMin]) // 빈 배열을 의존성 목록으로 제공하여 마운트 시에만 effect 실행
+  }, [timer.hour, timer.min])
 
   return (
     <div className="bg-white bg-opacity-20 border border-accent border-opacity-50 flex p-2 w-fit rounded-3xl shadow-foggyPurple items-center mr-2">
