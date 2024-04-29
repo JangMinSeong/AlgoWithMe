f126dd905e76999ea3fa69d9823ed69995d72847
최재성 <jaisung0410@gmail.com>
2024-04-29 11:57:33 +0900

    [BE] Fix: 소켓 테스트 수정

---
+++ back/src/main/java/com/ssafy/Algowithme/common/entity/Greeting.java
@@ -0,0 +1,12 @@
+package com.ssafy.Algowithme.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
