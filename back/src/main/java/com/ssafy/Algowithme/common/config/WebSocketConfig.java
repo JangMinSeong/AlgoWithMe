a81c70f504b0d21a4b286e8feb57e6bfd96827b9
최재성 <jaisung0410@gmail.com>
2024-04-29 14:29:04 +0900

    [BE] Fix: CORS 설정, pub/sub 경로 변경

--- back/src/main/java/com/ssafy/Algowithme/common/config/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/Algowithme/common/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/algowithme-websocket").withSockJS(); // 커넥션을 맺는 경로 설정. 만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
+        registry.addEndpoint("/algowithme-websocket").setAllowedOrigins("http://k10d205.p.ssafy.io:3000/").withSockJS(); // 커넥션을 맺는 경로 설정. 만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
     }
 
 }
