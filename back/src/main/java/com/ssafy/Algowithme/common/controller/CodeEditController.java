ae225e28be84552c4a7bf464b851d0f387c2e14c
최재성 <jaisung0410@gmail.com>
2024-04-29 11:57:33 +0900

    [BE] Fix: 소켓 테스트 수정

--- back/src/main/java/com/ssafy/Algowithme/common/controller/CodeEditController.java
+++ back/src/main/java/com/ssafy/Algowithme/common/controller/CodeEditController.java
@@ -1,26 +1,30 @@
 package com.ssafy.Algowithme.common.controller;
 
-import com.ssafy.Algowithme.common.entity.EditCode;
+import com.ssafy.Algowithme.common.entity.Greeting;
+import com.ssafy.Algowithme.common.entity.HelloMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 @RestController
 @RequiredArgsConstructor
 public class CodeEditController {
+
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/code-edit/{pageId}/{codeId}")
+    @MessageMapping("/hello/{pageId}/{codeId}")
     @SendTo("/topic/greetings")
-    public String greeting(@PathVariable("pageId") int pageId, @PathVariable("codeId") int codeId, String code) throws Exception {
-        String destination = "/topic/study/" + pageId;
-        EditCode response = new EditCode(codeId, code);
-        messagingTemplate.convertAndSend(destination, response);
+    public Greeting test(@DestinationVariable("pageId") int pageId, @DestinationVariable("codeId") int codeId, HelloMessage message) throws Exception {
+        System.out.println("pageId: " + pageId + ", codeId: " + codeId);
+//        String destination = "/topic/study/" + pageId;
+//        messagingTemplate.convertAndSend(destination, new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!"));
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 }
 
