98ac41457510409cf92e99d4aad49e097dd4d0e5
JIYEON KIM <jiyeon2536@naver.com>
2024-04-30 14:07:46 +0900

    [FE] Feat : 스터디룸 생성 링크

--- front/components/studypage/EnterStudyRoom.tsx
+++ front/components/studypage/EnterStudyRoom.tsx
@@ -11,10 +11,13 @@ const EnterStudyRoom = () => {
           아직 생성된 방이 없어요
         </div>
       </div>
-      <div className="w-60 h-72 bg-white shadow-foggyBlue rounded-md flex flex-col justify-center items-center hover:bg-purple-200 hover:border-opacity-0 transition-colors">
+      <Link
+        href={`/study/${'방번호'}/room`}
+        className="w-60 h-72 bg-white shadow-foggyBlue rounded-md flex flex-col justify-center items-center hover:bg-purple-200 hover:border-opacity-0 transition-colors"
+      >
         <TiDocumentAdd className="w-6 h-6" />
         <div className="mt-4 font-semibold">스터디룸 생성하기</div>
-      </div>
+      </Link>
       {/* <div className="flex flex-col justify-center items-center mb-4 text-neutral-500">
         이미 생성된 방이 있어요
       </div>
