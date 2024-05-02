6ae8e5e524af713c9b3d5df311f9b4dc3cd40822
최재성 <jaisung0410@gmail.com>
2024-05-02 10:16:40 +0900

    [BE] Feat: 팀 생성

--- back/src/main/java/com/ssafy/Algowithme/team/controller/TeamController.java
+++ back/src/main/java/com/ssafy/Algowithme/team/controller/TeamController.java
@@ -1,7 +1,7 @@
 package com.ssafy.Algowithme.team.controller;
 
+import com.ssafy.Algowithme.team.dto.request.CreateTeamRequest;
 import com.ssafy.Algowithme.team.dto.request.ProblemAddRequest;
-import com.ssafy.Algowithme.team.entity.CandidateProblem;
 import com.ssafy.Algowithme.team.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +9,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/team")
+@RequestMapping("/study")
 public class TeamController {
 
     private final TeamService teamService;
+
+    @PostMapping
+    public ResponseEntity<?>  createTeam(CreateTeamRequest request) {
+        return teamService.createTeam(request);
+    }
 
     @PostMapping("/problem")
     public ResponseEntity<?> addCandidateProblem(ProblemAddRequest request) {
         return teamService.addCandidateProblem(request);
     }
 
-//    @DeleteMapping("/problem")
-//    public ResponseEntity<?> deleteCandidateProblem(ProblemDeleteRequest request) {
-//        return teamService.deleteCandidateProblem(request);
-//    }
 }
--- back/src/main/java/com/ssafy/Algowithme/team/service/TeamService.java
+++ back/src/main/java/com/ssafy/Algowithme/team/service/TeamService.java
@@ -4,11 +4,17 @@ import com.ssafy.Algowithme.common.exception.CustomException;
 import com.ssafy.Algowithme.common.exception.ExceptionStatus;
 import com.ssafy.Algowithme.problem.entity.Problem;
 import com.ssafy.Algowithme.problem.repository.ProblemRepository;
+import com.ssafy.Algowithme.team.dto.request.CreateTeamRequest;
 import com.ssafy.Algowithme.team.dto.request.ProblemAddRequest;
+import com.ssafy.Algowithme.team.dto.response.TeamInfoResponse;
 import com.ssafy.Algowithme.team.entity.CandidateProblem;
 import com.ssafy.Algowithme.team.entity.Team;
 import com.ssafy.Algowithme.team.repository.CandidateProblemRepository;
 import com.ssafy.Algowithme.team.repository.TeamRepository;
+import com.ssafy.Algowithme.user.entity.User;
+import com.ssafy.Algowithme.user.entity.UserTeam;
+import com.ssafy.Algowithme.user.repository.UserRepository;
+import com.ssafy.Algowithme.user.repository.UserTeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,10 +24,35 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TeamService {
 
+    private final UserRepository userRepository;
+    private final UserTeamRepository userTeamRepository;
     private final TeamRepository teamRepository;
     private final ProblemRepository problemRepository;
     private final CandidateProblemRepository candidateProblemRepository;
 
+
+    public ResponseEntity<?> createTeam(CreateTeamRequest request) {
+        //유저 조회
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new CustomException(ExceptionStatus.USER_NOT_FOUND));
+
+        //팀 생성
+        Team team = teamRepository.save(Team.builder()
+                        .name(request.getName())
+                        .description(request.getDescription())
+                        .canRead(false)
+                        .imageUrl(request.getImageUrl())
+                        .build());
+
+        //팀멤버 저장
+        userTeamRepository.save(UserTeam.builder()
+                        .user(user)
+                        .team(team)
+                        .manager(true)
+                        .build());
+
+        return ResponseEntity.ok(TeamInfoResponse.create(team.getId(), request));
+    }
 
     public ResponseEntity<?> addCandidateProblem(ProblemAddRequest request) {
         //팀 조회
