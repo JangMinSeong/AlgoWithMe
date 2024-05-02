3e851c442fb3dc0d4c55e80c5b89a7611655be38
박성진 <p7364009@gmail.com>
2024-05-02 13:16:08 +0900

    [BE] Fix: 토큰 리프레시 response 변경 완료

--- back/src/main/java/com/ssafy/Algowithme/common/exception/ExceptionStatus.java
+++ back/src/main/java/com/ssafy/Algowithme/common/exception/ExceptionStatus.java
@@ -13,6 +13,7 @@ public enum ExceptionStatus {
     REFRESH_TOKEN_IS_NOT_VALID(1002, "Refresh Token이 유효하지 않습니다."),
     USER_MISMATCH(1003, "사용자가 일치하지 않습니다."),
     USER_NOT_FOUND(1004, "유저가 존재하지 않습니다."),
+    AUTHORIZATION_USER_NOT_FOUNT(1005, "Authorization에 해당하는 사용자를 찾을 수 없습니다."),
 
     // team(11)
     TEAM_NOT_FOUND(1100, "팀이 존재하지 않습니다."),
