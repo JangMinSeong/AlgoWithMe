df89720d8b2099f7ad1255ff90eb9e355b4053e3
조현우 <chw03177@gmail.com>
2024-05-03 09:46:46 +0900

    [FE] !Hotfix: 미들웨어 버그 수정

--- front/middleware.ts
+++ front/middleware.ts
@@ -4,6 +4,8 @@ export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   const token = request.cookies.get('algowithme_refreshToken')
 
+  const permitPaths = ['/main', '/study', '/editor']
+
   if (process.env.NODE_ENV === 'development') return NextResponse.next()
 
   if (pathname === '/') {
@@ -14,10 +16,20 @@ export default function middleware(request: NextRequest) {
     }
   }
 
-  if (pathname !== '/' && !token) {
-    const url = request.nextUrl.clone()
-    url.pathname = '/'
-    return NextResponse.redirect(url)
+  const isRedirected = permitPaths.some((path) => {
+    if (pathname.startsWith(path)) {
+      if (!token) {
+        const url = request.nextUrl.clone()
+        url.pathname = '/'
+        NextResponse.redirect(url)
+        return true
+      }
+    }
+    return false
+  })
+
+  if (isRedirected) {
+    return NextResponse.redirect('/')
   }
 
   return NextResponse.next()
