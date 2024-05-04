f69f9279bf666aab3c7fad4189f5a74a8bf0381a
sarang682 <jms20914@gmail.com>
2024-05-04 16:36:55 +0900

    [BE] Fix: 페이지 태그 종류 업데이트

--- back/src/main/java/com/ssafy/Algowithme/problem/type/Tag.java
+++ back/src/main/java/com/ssafy/Algowithme/problem/type/Tag.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Tag {
     DFS("DFS"), BFS("BFS"), BRUTEFORCE("브루트포스"),
-    GREEDY("그리디"), SHORTEST_PATH("최단경로"), ;
+    GREEDY("그리디"), DP("다이나믹 프로그래밍"),
+    STRING("문자열"), BINARY_SEARCH("이분 탐색"), SIMULATION("구현"),
+    SORTING("정렬"), BITMASK("비트 마스킹"), BACKTRACKING("백트래킹"),
+    DATA_STRUCTURES("자료구조"), GRAPH("그래프"), ;
     final private String name;
 }
