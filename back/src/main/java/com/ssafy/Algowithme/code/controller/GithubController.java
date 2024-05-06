924a76e8aa577d3250bf72a8aad2796abcfc8c61
sarang682 <jms20914@gmail.com>
2024-05-06 16:51:01 +0900

    [BE] Feat: 깃허브 리포지토리 이름으로 조회

--- back/src/main/java/com/ssafy/Algowithme/code/controller/GithubController.java
+++ back/src/main/java/com/ssafy/Algowithme/code/controller/GithubController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,12 @@ public class GithubController {
     private final GithubService githubService;
 
     @GetMapping("/repository/list")
-    public ResponseEntity<List<RepositoryResponse>> list(@AuthenticationPrincipal User user) {
+    public ResponseEntity<List<RepositoryResponse>> listRepositories(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(githubService.getListRepositoriesForUser(user));
+    }
+
+    @GetMapping("/repository/{name}")
+    public ResponseEntity<RepositoryResponse> getRepository(@PathVariable String name, @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(githubService.getRepositoryForUser(name, user));
     }
 }
--- back/src/main/java/com/ssafy/Algowithme/code/service/GithubService.java
+++ back/src/main/java/com/ssafy/Algowithme/code/service/GithubService.java
@@ -4,7 +4,6 @@ import com.ssafy.Algowithme.code.dto.response.RepositoryResponse;
 import com.ssafy.Algowithme.common.exception.CustomException;
 import com.ssafy.Algowithme.common.exception.ExceptionStatus;
 import com.ssafy.Algowithme.user.entity.User;
-import com.ssafy.Algowithme.user.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
@@ -20,17 +19,35 @@ import java.util.Map;
 public class GithubService {
 
     public List<RepositoryResponse> getListRepositoriesForUser(User user) {
+        GitHub gitHub = getGitHub(user.getGitToken());
         try {
-            GitHub gitHub = new GitHubBuilder().withOAuthToken(user.getGitToken()).build();
-            gitHub.checkApiUrlValidity();
             Map<String, GHRepository> repositories = gitHub.getMyself().getRepositories();
             return repositories.keySet().stream()
                     .map(repositories::get)
                     .map(RepositoryResponse::create)
                     .toList();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new CustomException(ExceptionStatus.GITHUB_ACCESS_DENIED);
+        }
+    }
+
+    public RepositoryResponse getRepositoryForUser(String name, User user) {
+        GitHub gitHub = getGitHub(user.getGitToken());
+        try {
+            return RepositoryResponse.create(gitHub.getMyself().getRepository(name));
+        } catch (IOException e) {
+            throw new CustomException(ExceptionStatus.GITHUB_ACCESS_DENIED);
+        }
+    }
+
+    public GitHub getGitHub(String token) {
+        try {
+            GitHub gitHub = new GitHubBuilder().withOAuthToken(token).build();
+            gitHub.checkApiUrlValidity();
+            return gitHub;
+        } catch (IOException e) {
             throw new CustomException(ExceptionStatus.GITHUB_TOKEN_BAD_CREDENTIALS);
         }
     }
+
 }
