cbc820ddd455b122e878178f3426a2f1e188e1ed
박성진 <p7364009@gmail.com>
2024-05-08 13:15:43 +0900

    [BE] Feat: 스터디 그룹 메인페이지 정보 조회 시 사용자의 방문 일자를 최신화하는 기능 추가

--- back/src/main/java/com/ssafy/Algowithme/team/service/TeamService.java
+++ back/src/main/java/com/ssafy/Algowithme/team/service/TeamService.java
@@ -5,7 +5,6 @@ import com.ssafy.Algowithme.common.exception.CustomException;
 import com.ssafy.Algowithme.common.exception.ExceptionStatus;
 import com.ssafy.Algowithme.problem.entity.Problem;
 import com.ssafy.Algowithme.problem.repository.ProblemRepository;
-import com.ssafy.Algowithme.team.dto.request.CreateTeamRequest;
 import com.ssafy.Algowithme.team.dto.request.ProblemAddRequest;
 import com.ssafy.Algowithme.team.dto.response.TeamInfoDetailResponse;
 import com.ssafy.Algowithme.team.dto.response.TeamInfoResponse;
@@ -17,14 +16,12 @@ import com.ssafy.Algowithme.user.entity.User;
 import com.ssafy.Algowithme.user.entity.UserTeam;
 import com.ssafy.Algowithme.user.repository.UserTeamRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -92,12 +89,16 @@ public class TeamService {
         userTeamRepository.findByUserAndTeam(user, team).orElseGet(UserTeam::new);
     }
 
+    @Transactional
     public TeamInfoDetailResponse getTeamInfoDetail(User user, Long teamId) {
         UserTeam userTeam = userTeamRepository.findByUserIdAndTeamId(user.getId(), teamId)
                 .orElseThrow(() -> new CustomException(ExceptionStatus.USER_TEAM_NOT_FOUND));
 
         Team team = teamRepository.findById(teamId)
                 .orElseThrow(() -> new CustomException(ExceptionStatus.TEAM_NOT_FOUND));
+
+        userTeam.setVisitedAt(LocalDateTime.now());
+        userTeamRepository.save(userTeam);
 
         return TeamInfoDetailResponse.builder()
                 .teamId(teamId)
