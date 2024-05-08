a9e1d7cc9892166e9f70308c5255d7adf4f09638
JIYEON KIM <jiyeon2536@naver.com>
2024-05-08 14:46:32 +0900

    [FE] Feat : 스터디 메인페이지 기능 수정

--- front/components/Header/Timer.tsx
+++ front/components/Header/Timer.tsx
@@ -4,6 +4,9 @@ import { useSelector } from 'react-redux'
 import { RootState } from '@/lib/store'
 import { useEffect, useState } from 'react'
 import toast, { Toaster } from 'react-hot-toast'
+import { FaRegCirclePause } from 'react-icons/fa6'
+import { FaRegPlayCircle, FaRegStopCircle } from 'react-icons/fa'
+import useSolving from '@/hooks/useSolving'
 
 const Timer = () => {
   const timer = useSelector((state: RootState) => state.timer)
@@ -16,10 +19,23 @@ const Timer = () => {
 
   const isSolving = useSelector((state: RootState) => state.solving.isSolving)
 
-  useEffect(() => {
-    const startTime = Number(localStorage.getItem('startedAt'))
+  const { handleStartSolving, handleEndSolving } = useSolving()
 
+  const handleStart = () => {
+    const solvingStartTime = new Date().getTime()
+    localStorage.setItem('startedAt', String(solvingStartTime))
+    handleStartSolving()
+  }
+  const handleEnd = () => {
+    if (confirm('풀이를 종료하시겠어요?')) {
+      handleEndSolving()
+      localStorage.removeItem('startedAt')
+    }
+  }
+
+  useEffect(() => {
     if (isSolving) {
+      const startTime = Number(localStorage.getItem('startedAt'))
       const timerID = setInterval(() => {
         const localTime = new Date().getTime()
         // 매 초마다 현재 로컬 시간 업데이트
@@ -43,14 +59,15 @@ const Timer = () => {
         setRemainMin(Math.max(0, newRemainMin))
         setRemainSec(Math.max(0, newRemainSec))
       }, 1000) // 1초 간격으로 setInterval 실행
+    } else {
+      setRemainHour(Math.max(0, timer.hour))
+      setRemainMin(Math.max(0, timer.min))
+      setRemainSec(0)
     }
-    setRemainHour(Math.max(0, timer.hour))
-    setRemainMin(Math.max(0, timer.min))
-    setRemainSec(0)
-  }, [timer.hour, timer.min])
+  }, [timer.hour, timer.min, remainHour, remainMin, remainSec])
 
   return (
-    <div>
+    <div className="flex items-center">
       <div
         className={`bg-white bg-opacity-20 border border-accent border-opacity-50 flex p-2 w-fit rounded-3xl shadow-foggyPurple items-center mr-2`}
       >
@@ -60,9 +77,21 @@ const Timer = () => {
         {remainMin}
         <span className="text-xs text-navy mr-2 ml-1">분</span>
         {remainSec}
-        <span className="text-xs text-navy ml-1">초</span>
-        <Toaster position="bottom-center" reverseOrder={false} />
+        <span className="text-xs text-navy ml-1 mr-2">초</span>
+
+        <div className="flex">
+          {isSolving ? (
+            <FaRegStopCircle onClick={handleEnd} />
+          ) : (
+            <FaRegPlayCircle
+              className="text-red-500 mr-1"
+              onClick={handleStart}
+            />
+          )}
+        </div>
       </div>
+
+      <Toaster position="bottom-center" reverseOrder={false} />
     </div>
   )
 }
