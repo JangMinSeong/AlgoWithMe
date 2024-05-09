1f3c1c8aa8760fb6218404385343718db812347f
조현우 <chw03177@gmail.com>
2024-05-09 16:05:49 +0900

    [FE] Feat: redux-persist로 access token 로컬에 저장

--- front_react/src/StoreProvider.tsx
+++ front_react/src/StoreProvider.tsx
@@ -1,7 +1,6 @@
-'use client'
-
 import React, { useRef } from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppStore, makeStore } from '@/lib/store'
 
 export default function StoreProvider({
@@ -9,10 +8,15 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode
 }) {
-  const storeRef = useRef<AppStore>()
+    const storeRef = useRef<{ store: ReturnType<typeof makeStore>['store'], persistor: ReturnType<typeof makeStore>['persistor'] }>();
   if (!storeRef.current) {
     storeRef.current = makeStore()
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>
+  return (
+      <Provider store={storeRef.current.store}>
+        <PersistGate persistor={storeRef.current.persistor}>
+          {children}
+        </PersistGate>
+      </Provider>)
 }
