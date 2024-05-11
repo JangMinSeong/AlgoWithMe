1249b0ba1c60dda04578b456f6339dbc24b9dd41
장민성 <hs0765@naver.com>
2024-05-11 23:57:10 +0900

    [FE] Feat: 폴더 구조 변경 및 소켓 연결 완

---
+++ back/src/main/java/com/ssafy/Algowithme/code/dto/request/SocketMessage.java
@@ -0,0 +1,13 @@
+package com.ssafy.Algowithme.code.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SocketMessage {
+    private String language;
+    private String code;
+}
