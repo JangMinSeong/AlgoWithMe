2e2fdf398e4199cb8027ede69e6d1f6aa52fc3d4
JIYEON KIM <jiyeon2536@naver.com>
2024-05-13 14:04:36 +0900

    [FE] Fix : 오픈비두 테스트 필요

--- front_react/src/components/header/Avatar.tsx
+++ front_react/src/components/header/Avatar.tsx
@@ -1,25 +1,37 @@
 import { RootState } from '@/lib/store'
 import { useSelector } from 'react-redux'
-import useCode from "@/hooks/useCode.ts";
-import {useWebSocket} from "@/hooks/useWebSocket.ts";
+import useCode from '@/hooks/useCode.ts'
+import { useWebSocket } from '@/hooks/useWebSocket.ts'
+import { useState } from 'react'
+import useAuth from '@/hooks/useAuth'
 
 interface UserInfo {
-    id:number
-    nickname:string
-    imageUrl:string
+  id: number
+  nickname: string
+  imageUrl: string
 }
 
-
-const Avatar = ( props : { userInfo: UserInfo, isProfile:boolean }) => {
+const Avatar = (props: { userInfo: UserInfo; isProfile: boolean }) => {
   //const userImage = useSelector((state: RootState) => state.auth.user.imageUrl)
-    const {handleCurUserId} = useCode()
-    const curUser =  useSelector((state: RootState) => state.code.curUserId)
-    const handleClickEvent = () => {
-        handleCurUserId(props.userInfo.id)
+  const { handleLogout } = useAuth()
+  const [showLogOutButton, setShowLogOutButton] = useState(false)
+  const { handleCurUserId } = useCode()
+  const curUser = useSelector((state: RootState) => state.code.curUserId)
+  const handleClickEvent = () => {
+    handleCurUserId(props.userInfo.id)
+    if (props.isProfile) {
+      setShowLogOutButton(!showLogOutButton)
     }
+  }
 
   return (
-    <div className={`${curUser === props.userInfo.id && !props.isProfile ? "border border-red-500 rounded-full" : ""}`}>
+    <div
+      className={`${
+        curUser === props.userInfo.id && !props.isProfile
+          ? 'border border-red-500 rounded-full'
+          : ''
+      }`}
+    >
       <img
         src={props.userInfo.imageUrl}
         alt="프로필"
@@ -28,6 +40,9 @@ const Avatar = ( props : { userInfo: UserInfo, isProfile:boolean }) => {
         className="rounded-full"
         onClick={handleClickEvent}
       />
+      {showLogOutButton && (
+        <button onClick={() => handleLogout()}>로그아웃</button>
+      )}
     </div>
   )
 }
