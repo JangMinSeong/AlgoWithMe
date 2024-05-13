9a6b86d04707134089d883ba77c0cab506feabc3
조현우 <chw03177@gmail.com>
2024-05-13 10:32:19 +0900

    [FE] Fix: 로고 컴포넌트 통일

--- front_react/src/components/Logo.tsx
+++ front_react/src/components/Logo.tsx
@@ -2,15 +2,18 @@ import LogoImg from '/logo.svg'
 
 interface LogoProps {
   className?: string
+  width?: number
 }
 
-export default function Logo({ className }: LogoProps) {
+export default function Logo({ className, width }: LogoProps) {
   return (
     <img
       src={LogoImg}
       alt="Logo"
-      className={`hover:cursor-pointer ${className || ''}`}
-      width={80}
+      className={`transition ease-in-out hover:cursor-pointer hover:drop-shadow-md hover:duration-300 hover:scale-105 ${
+        className || ''
+      }`}
+      width={width || 80}
       onClick={() => {
         window.location.href = '/'
       }}
--- front_react/src/components/header/StudyHeader.tsx
+++ front_react/src/components/header/StudyHeader.tsx
@@ -1,67 +1,65 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import Logo from '/logo.svg'
+import Logo from '@/components/Logo'
 import Avatar from './Avatar'
 import Timer from './Timer'
 import SideBarButton from '../sidebar/SideBarButton'
 import GroupCall from '../groupcall/GroupCall'
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react'
 import fetch from '@/lib/fetch.ts'
-import {setPageList} from "@/features/sidebar/sidebarSlice.ts";
-import {RootState} from "@/lib/store.ts";
-import useCode from "@/hooks/useCode.ts";
+import { RootState } from '@/lib/store.ts'
+import useCode from '@/hooks/useCode.ts'
 
 interface UserInfo {
-    id:number
-    nickname:string
-    imageUrl:string
+  id: number
+  nickname: string
+  imageUrl: string
 }
 
 const StudyHeader = (props: { groupId: number }) => {
-    const [users, setUsers] = useState<UserInfo[]>([]);
-    const [curUser, setCurUser] = useState<UserInfo | null>(null);
-    const nickname = useSelector((state: RootState) => state.auth.user.nickname); // 현재 사용자 닉네임 가져오기
-    const {handleUserList,handleMyId, handleCurUserId} = useCode()
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const response = await fetch(`/study/${props.groupId}/members`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            if (response.ok) {
-                const responseData = await response.json();
-                setUsers(responseData);
+  const [users, setUsers] = useState<UserInfo[]>([])
+  const [curUser, setCurUser] = useState<UserInfo | null>(null)
+  const nickname = useSelector((state: RootState) => state.auth.user.nickname) // 현재 사용자 닉네임 가져오기
+  const { handleUserList, handleMyId, handleCurUserId } = useCode()
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(`/study/${props.groupId}/members`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (response.ok) {
+        const responseData = await response.json()
+        setUsers(responseData)
 
-                // 현재 사용자 닉네임과 일치하는 사용자 찾기
-                const foundUser = responseData.find((user: UserInfo) => user.nickname === nickname);
-                setCurUser(foundUser || null);
-                handleMyId(foundUser.id)
-                handleUserList(responseData)
-                if(curUser === 0) handleCurUserId(foundUser.id)
-            }
-        };
-        fetchUsers();
-    }, [props.groupId, nickname]);
+        // 현재 사용자 닉네임과 일치하는 사용자 찾기
+        const foundUser = responseData.find(
+          (user: UserInfo) => user.nickname === nickname,
+        )
+        setCurUser(foundUser || null)
+        handleMyId(foundUser.id)
+        handleUserList(responseData)
+        if (curUser === 0) handleCurUserId(foundUser.id)
+      }
+    }
+    fetchUsers()
+  }, [props.groupId, nickname])
 
   return (
     <div className="fixed z-10 top-2 left-2 w-[98vw] h-12 flex justify-between items-center bg-white bg-opacity-50 rounded-xl px-5">
       <div className="flex items-center w-1/4">
         <SideBarButton />
-        <Link to="/main">
-          <img src={Logo} alt="로고" width={80} height={0} />
-        </Link>
+        <Logo />
       </div>
       <div className="flex items-center">
-          {users.map((user) => (
-              <Avatar key={user.id} userInfo={user} isProfile={false}/>
-          ))}
+        {users.map((user) => (
+          <Avatar key={user.id} userInfo={user} isProfile={false} />
+        ))}
         <GroupCall />
       </div>
       <div className="flex items-center">
         <Timer />
-          {curUser && <Avatar userInfo={curUser} isProfile={true}/>}
+        {curUser && <Avatar userInfo={curUser} isProfile={true} />}
       </div>
     </div>
   )
