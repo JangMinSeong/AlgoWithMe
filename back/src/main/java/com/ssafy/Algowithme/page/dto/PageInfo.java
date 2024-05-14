319258edf92b1236b826434cf283691b95648fe4
최재성 <jaisung0410@gmail.com>
2024-05-14 16:04:39 +0900

    [BE] Fix: 스터디 내 페이지 조회 (pageInfo 에 provider 추가)

--- back/src/main/java/com/ssafy/Algowithme/page/dto/PageInfo.java
+++ back/src/main/java/com/ssafy/Algowithme/page/dto/PageInfo.java
@@ -1,6 +1,7 @@
 package com.ssafy.Algowithme.page.dto;
 
 import com.ssafy.Algowithme.page.entity.Page;
+import com.ssafy.Algowithme.problem.type.Provider;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ public class PageInfo {
     private Long pageId;
     private String title;
     private boolean isDocs;
+    private String provider;
     private List<PageInfo> children;
 
     public static PageInfo create(Page page) {
@@ -22,10 +24,17 @@ public class PageInfo {
         if(page.getProblem() == null) {
             isDocs = true;
         }
+
+        String provider = "";
+        if(isDocs == false) {
+            provider = page.getProblem().getProvider().getName();
+        }
+
         return PageInfo.builder()
                 .pageId(page.getId())
                 .title(page.getTitle())
                 .isDocs(isDocs)
+                .provider(provider)
                 .children(new ArrayList<>())
                 .build();
     }
