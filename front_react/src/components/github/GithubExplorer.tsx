58ff9ebcab84266f9c720beaafe69061ba48caea
sarang682 <jms20914@gmail.com>
2024-05-15 15:11:09 +0900

    [FE] Fix: 깃허브 업로드 수정

--- front_react/src/components/github/GithubExplorer.tsx
+++ front_react/src/components/github/GithubExplorer.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import fetch from '@/lib/fetch.ts'
 import BranchExplorer from "@/components/github/BranchExplorer.tsx";
 import DirectoryExplorer from '@/components/github/DirectoryExplorer.tsx'
+import GithubUpload from '@/components/github/GithubUpload.tsx'
 
 interface Repository {
     name: string
@@ -13,38 +14,12 @@ interface Repository {
 const GitHubExplorer = ({isOpen, isClose, repositories, content, language}) => {
     const [branches, setBranches] = useState<string[]>([])
     const [activeRepo, setActiveRepo] = useState<string | null>(null);
-    const [selectedDirectoryPath, setSelectedDirectoryPath] = useState( "");
     const [activeBranch, setActiveBranch] = useState(null)
-    const [commitMessage, setCommitMessage] = useState("")
-    const [filename, setFilename] = useState("")
-    const [isSave, setIsSave] = React.useState(false)
     const [naviText, setNaviTest] = useState("리포지토리 선택")
-    // 디렉토리
     const [path, setPath] = useState("");
     const [directories, setDirectories] = useState<string[]>([])
+    const [activeUpload, setActiveUpload] = useState(false)
 
-    const handleSaveButton = async () => {  //TODO: 깃 저장 호출
-        const dataToSave = {
-            repo : activeRepo,
-            branch :activeBranch,
-            path : `${selectedDirectoryPath}` ? `${selectedDirectoryPath}/` : "" ,
-            fileName : filename,
-            language,
-            content,
-            commitMessage:commitMessage
-        }
-        if(filename && commitMessage) {
-            await fetch(`/github/upload`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(dataToSave)
-            });
-            isClose()
-        }
-        else alert("파일명과 커밋 메시지를 작성해주세요")
-    }
 
     const handleBranchSelect = (branch:string) => {
         setActiveBranch(branch);
@@ -52,9 +27,7 @@ const GitHubExplorer = ({isOpen, isClose, repositories, content, language}) => {
 
     const handleRepoClick = async (repo:Repository) => {
         setActiveRepo(activeRepo === repo.name ? null : repo.name);
-        setSelectedDirectoryPath("")
-        setActiveBranch("")
-
+        setActiveBranch(null)
         const dataToPost = {
             repo:repo.name
         }
@@ -87,6 +60,10 @@ const GitHubExplorer = ({isOpen, isClose, repositories, content, language}) => {
             setNaviTest("디렉토리 선택")
     }, [activeRepo, activeBranch])
 
+    const handleSaveBtn = () => {
+        setActiveUpload(true)
+    }
+
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4 z-50">
             <div className="bg-white rounded-lg shadow-lg max-w-3xl w-full overflow-hidden">
@@ -114,44 +91,8 @@ const GitHubExplorer = ({isOpen, isClose, repositories, content, language}) => {
                                         onBranchSelect={handleBranchSelect} setDirectories={setDirectories} />) : null
                     }
                 </div>
-                { activeBranch && <button>저장</button>} <button onClick={() => isClose()}>닫기</button>
-                {/*<div className={'flex flex-col justify-end border-t border-gray-200 mx-3'}>*/}
-                {/*    <div className={'mr-5'}>*/}
-                {/*        {activeBranch &&*/}
-                {/*          <textarea*/}
-                {/*            className={'border border-gray-300 py-2 mt-3 w-full pl-5 ml-2 resize-none overflow-auto'}*/}
-                {/*            placeholder={'커밋 메시지를 입력해주세요'}*/}
-                {/*            value={commitMessage}*/}
-                {/*            onChange={(e) => setCommitMessage(e.target.value)}*/}
-                {/*          ></textarea>*/}
-                {/*        }*/}
-                {/*    </div>*/}
-                {/*    <div className={`mb-3 w-full flex ${activeBranch ? 'justify-between' : 'justify-end'}`}>*/}
-                {/*        {activeBranch && (*/}
-                {/*          <>*/}
-                {/*                <input*/}
-                {/*                    className="w-full pl-5 border border-gray-300 ml-2"*/}
-                {/*                    placeholder="파일명을 입력해주세요"*/}
-                {/*                    value={filename}*/}
-                {/*                    onChange={(e) => setFilename(e.target.value)}*/}
-                {/*                />*/}
-                {/*                <div className = {"text-sm text-gray-500 pr-10 pt-5 pl-2"}>.{language}</div>*/}
-                {/*                <div className="text-right pr-2">*/}
-                {/*                    <button onClick={handleSaveButton}*/}
-                {/*                            className="py-2 px-4 bg-fuchsia-300 text-white rounded hover:bg-navy">*/}
-                {/*                        Save*/}
-                {/*                    </button>*/}
-                {/*                </div>*/}
-                {/*            </>*/}
-                {/*        )}*/}
-                {/*        <div className="text-right">*/}
-                {/*            <button onClick={isClose}*/}
-                {/*                    className="py-2 px-4 bg-gray-500 text-white rounded hover:bg-gray-600">*/}
-                {/*                Close*/}
-                {/*            </button>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                { activeBranch && <button onClick={handleSaveBtn}>저장</button>} <button onClick={() => isClose()}>닫기</button>
+                {activeUpload && <GithubUpload content={content} language={language} setActiveUpload={setActiveUpload} activeRepo={activeRepo} activeBranch={activeBranch} path={path}/> }
             </div>
         </div>
     );
---
+++ front_react/src/components/github/GithubUpload.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import fetch from '@/lib/fetch.ts'
+
+interface uploadProp {
+  activeRepo: string,
+  activeBranch: string,
+  path: string,
+  content: string
+  language: string
+  setActiveUpload: (arg: boolean) => void;
+}
+
+const GithubUpload: React.FC<uploadProp> = ({ content, language, setActiveUpload, activeRepo, activeBranch, path}) => {
+  const [commitMessage, setCommitMessage] = useState('')
+  const [filename, setFilename] = useState('')
+
+  const handleSaveButton = async () => {  //TODO: 깃 저장 호출
+    const dataToSave = {
+      repo : activeRepo,
+      branch :activeBranch,
+      path : `${path}`,
+      fileName : filename,
+      language,
+      content,
+      commitMessage:commitMessage
+    }
+    if(filename && commitMessage) {
+      await fetch(`/github/upload`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSave)
+      });
+      setActiveUpload(false)
+    }
+    else alert("파일명과 커밋 메시지를 작성해주세요")
+  }
+
+  return (
+    <div className={'fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4 z-50'}>
+      <div className={'flex flex-col justify-end border-t border-gray-200 mx-3 bg-white rounded-xl'}>
+        <div className={'mr-5'}>
+            <textarea
+              className={'border border-gray-300 py-2 mt-3 w-full pl-5 ml-2 resize-none overflow-auto'}
+              placeholder={'커밋 메시지를 입력해주세요'}
+              value={commitMessage}
+              onChange={(e) => setCommitMessage(e.target.value)}
+            ></textarea>
+        </div>
+        <div className={`mb-3 w-full flex justify-end`}>
+          <>
+            <input
+              className="w-full pl-5 border border-gray-300 ml-2"
+              placeholder="파일명을 입력해주세요"
+              value={filename}
+              onChange={(e) => setFilename(e.target.value)}
+            />
+            <div className={'text-sm text-gray-500 pr-10 pt-5 pl-2'}>.{language}</div>
+            <div className="text-right pr-2">
+              <button onClick={handleSaveButton}
+                      className="py-2 px-4 bg-fuchsia-300 text-white rounded hover:bg-navy">
+                저장
+              </button>
+            </div>
+            <div className="text-right pr-2">
+              <button onClick={() => setActiveUpload(false)}
+                      className="py-2 px-4 bg-fuchsia-300 text-white rounded hover:bg-navy">
+                취소
+              </button>
+            </div>
+          </>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GithubUpload
