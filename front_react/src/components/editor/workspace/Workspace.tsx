db0f86271ad5fbb51b7c9d99351d82185abd8989
장민성 <hs0765@naver.com>
2024-05-16 17:09:32 +0900

    [FE] Fix: 워크스페이스 메뉴바 수정

--- front_react/src/components/editor/workspace/Workspace.tsx
+++ front_react/src/components/editor/workspace/Workspace.tsx
@@ -1,20 +1,27 @@
 import '@/components/editor/workspace/styles.scss'
 
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Editor, EditorContent } from '@tiptap/react'
 
 import MenuBar from '@/components/editor/workspace/MenuBar'
+import editor from "@/pages/Editor.tsx";
 
 interface WorkSpaceProps {
   editor: Editor
   pageId: string
 }
 
-const WorkSpace: React.FC<WorkSpaceProps> = ({ editor, pageId }) => (
+const WorkSpace: React.FC<WorkSpaceProps> = ({ editor, pageId }) => {
+    const [curEditor, setCurEditor] = useState<Editor|null>(null)
+    useEffect ( () => {
+        setCurEditor(editor)
+    },[editor])
+    return (
   <div className="editor w-full h-full">
-    {editor && <MenuBar editor={editor} pageId={pageId} />}
-    <EditorContent className="editor__content" editor={editor} />
+    {curEditor && <MenuBar editor={curEditor} pageId={pageId} />}
+    <EditorContent className="editor__content" editor={curEditor} />
   </div>
-)
+    )
+}
 
 export default WorkSpace
